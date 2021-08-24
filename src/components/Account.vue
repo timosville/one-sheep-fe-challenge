baa52090672013242f4578ab155b563b66127456
Timothy Koech <timosville@gmail.com>
2021-08-24 14:19:47 +0300

    Add hero

--- src/components/Account.vue
+++ src/components/Account.vue
@@ -1,17 +1,44 @@
 <template>
-  <h1>Account</h1>
-  <h2>{{ store.state.name }}</h2>
-  <h2>Code: {{ store.state.code }}</h2>
+  <div class="min-h-screen flex flex-col sm:flex-row justify-between">
+    <div class="flex flex-col items-center justify-items-center">
+      <h1 class="sm:mt-20 pt-24 px-8 title">
+        Hey {{ store.state.name }}, <br />let’s create your account.
+      </h1>
+      <input type="email" class="field" placeholder="Email" v-model="email" />
+
+      <input
+        type="password"
+        class="field"
+        placeholder="Password"
+        v-model="password"
+      />
+
+      <button
+        @click="changeRoute()"
+        class="absolute bottom-0 mb-12 sm:mb-28 button"
+      >
+        Create
+      </button>
+    </div>
+    <div class="sm:flex-1 flex flex-col items-center justify-center">
+      <img class="sm:mx-3 sm:w-9/12" src="/hero/security.svg" alt="security" />
+    </div>
+  </div>
 </template>
 
+
 <script>
-import { inject, computed } from "vue";
+import { inject, ref } from "vue";
 
 export default {
   setup() {
     const store = inject("store");
+    const email = ref(null);
+    const password = ref(null);
     return {
       store,
+      email,
+      password,
     };
   },
 };
