b18e776f029046256471fd35d3eab28080227d5e
Timothy Koech <timosville@gmail.com>
2021-08-24 06:11:53 +0300

    Add and configure tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "one-yellow": "#FFDF70",
+        "one-green": "#93D85D",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
