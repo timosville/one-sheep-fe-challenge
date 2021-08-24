84337f800a1784677abd5bf028f5a4223d13acef
Timothy Koech <timosville@gmail.com>
2021-08-24 07:29:54 +0300

    Extract components: title & button

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -3,32 +3,11 @@
     class="min-h-screen bg-one-yellow flex flex-col sm:flex-row justify-between"
   >
     <div class="flex flex-col items-center justify-items-center">
-      <h1
-        class="sm:mt-20 pt-24 px-8 font-bold text-4xl text-center text-gray-900"
-      >
+      <h1 class="sm:mt-20 pt-24 px-8 title">
         Hi there.<br />
         Ready to do your best ever work?
       </h1>
-      <button
-        class="
-          absolute
-          bottom-0
-          mb-12
-          sm:mb-28
-          bg-white
-          rounded-full
-          px-8
-          py-3
-          shadow-md
-          text-center
-          font-extrabold
-          text-4xl
-          leading-tight
-          text-gray-900
-        "
-      >
-        Let's Go!
-      </button>
+      <button class="absolute bottom-0 mb-12 sm:mb-28 button">Let's Go!</button>
     </div>
     <div class="sm:flex-1 flex flex-col items-center justify-center">
       <img class="sm:mx-3 sm:w-9/12" src="/hero/welcome.svg" alt="welcome" />
