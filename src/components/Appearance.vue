2be7abeae8fd3ee9b84b9b54df3c2e28c0ba39a8
Timothy Koech <timosville@gmail.com>
2021-08-25 17:09:53 +0300

    Fix accessibility issue

--- src/components/Appearance.vue
+++ src/components/Appearance.vue
@@ -1,10 +1,20 @@
 <template>
-  <div class="min-h-screen flex flex-col sm:flex-row justify-between">
-    <div class="flex flex-col items-center justify-items-center">
-      <h1 class="sm:mt-20 pt-24 px-8 title">
+  <div class="max-h-screen container-grid">
+    <div class="container-grid__title">
+      <h1 class="title">
         Finally, let’s <br />
         customise your experience.
       </h1>
+    </div>
+
+    <div class="container-grid__hero">
+      <img
+        class="sm:mx-3 sm:w-9/12"
+        src="/hero/message-notification.svg"
+        alt="work at home"
+      />
+    </div>
+    <div class="container-grid__form">
       <h2 class="subtitle">Lights on or off?</h2>
       <button
         @click="store.methods.toggleLight"
@@ -25,13 +35,6 @@
         >
         </span>
       </button>
-    </div>
-    <div class="sm:flex-1 flex flex-col items-center justify-center">
-      <img
-        class="sm:mx-3 sm:w-9/12"
-        src="/hero/message-notification.svg"
-        alt="work at home"
-      />
     </div>
   </div>
 </template>
@@ -63,5 +66,3 @@ export default {
   },
 };
 </script>
-
-
