71fc10ad3b3a3b7c6b7f0eb7f2718dd3291440b4
Timothy Koech <timosville@gmail.com>
2021-08-27 08:08:46 +0300

    Add video url

--- src/components/Video.vue
+++ src/components/Video.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="bg-one-yellow max-h-screen container-grid">
-  
-    <div class="container-grid__hero">    
+    <div class="container-grid__hero">
       <img
         class="object-contain w-full"
         src="/hero/photos-and-media.svg"
         alt="media"
       />
     </div>
-     <div class="container-grid__video">
-            <img src="/screenshots/video.svg" alt="">
-        </div>
+    <div class="container-grid__video">
+      <video controls>
+        <source
+          src="https://res.cloudinary.com/kiekies/video/upload/v1629289654/mm3gvs6wmsg3zebylcqz.mp4"
+          type="video/mp4"
+          poster="/screenshots/video.svg"
+        />
+      </video>
+    </div>
   </div>
 </template>
 
