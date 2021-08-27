8f0755c33a4465746163c782e73451a4755163e5
Timothy Koech <timosville@gmail.com>
2021-08-27 08:00:23 +0300

    Align content

--- src/components/Video.vue
+++ src/components/Video.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="bg-one-yellow max-h-screen container-grid">
-    <div class="container-grid__hero">
+  
+    <div class="container-grid__hero">    
       <img
         class="object-contain w-full"
         src="/hero/photos-and-media.svg"
         alt="media"
       />
     </div>
+     <div class="container-grid__video">
+            <img src="/screenshots/video.svg" alt="">
+        </div>
   </div>
 </template>
 
